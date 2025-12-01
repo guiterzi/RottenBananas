1a73623166a144bddfa1de6d3ce609ed719159ee
guiterzi <127009910+guiterzi@users.noreply.github.com>
2025-12-01 20:32:20 -0300

    Update bancodedados.py

--- bancodedados.py
+++ bancodedados.py
@@ -3,7 +3,7 @@ import psycopg2
 def get_connection():
     return psycopg2.connect(
         host="localhost",
-        database="BANCOFILMES",
-        user="postgres",
-        password="123456"
+        database="banco",
+        user="usuario",
+        password="senha"
     )
